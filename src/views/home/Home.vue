d8f8912977c9aa36dea949764997be77d2bd65f8
hflag <532843488@qq.com>
2019-12-09 22:41:17 +0800

    '封装了NavBar组件，并加入home页面'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,25 @@
 <template>
-    <h2>主页</h2>
+  <div id="home">
+    <nav-bar class="nav-bar"><div slot="center">购物街</div></nav-bar>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "Home"
+  // import NavBar from 'components/common/navbar/BavBar'
+  import NavBar from 'components/common/navbar/NavBar'
+
+  export default {
+    name: "Home",
+    components:{
+      NavBar
     }
+  }
 </script>
 
 <style scoped>
-
+  .nav-bar{
+    background-color: var(--color-tint);
+    font-weight: 700;
+    color: #fff;
+  }
 </style>
