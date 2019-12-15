c02bac748eb039f2a52bd61146f8af54ee9e1a82
hflag <hflag@163.com>
2019-12-15 09:11:37 +0800

    '产品推荐组件完成'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,37 +4,41 @@
       <div slot="center">购物街</div>
     </nav-bar>
     <my-swipe :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
 
-  import NavBar from 'components/common/navbar/NavBar'
-  import MySwipe from 'components/common/swipe/MySwipe'
+    import NavBar from 'components/common/navbar/NavBar'
+    import MySwipe from 'components/common/swipe/MySwipe'
+    import RecommendView from "./childComps/RecommendView";
+    import {getHomeMultiData} from "../../network/home";
 
-  import {getHomeMultiData} from "../../network/home";
-
-  export default {
-    name: "Home",
-    data(){
-      return {
-        banners:[]
-      }
-    },
-    components:{
-      NavBar,
-      MySwipe
-    },
-    mounted() {
-      getHomeMultiData().then(res=>{
-        this.banners = res.data.data.banner.list
-      })
+    export default {
+        name: "Home",
+        data() {
+            return {
+                banners: [],
+                recommends: [],
+            }
+        },
+        components: {
+            RecommendView,
+            NavBar,
+            MySwipe
+        },
+        mounted() {
+            getHomeMultiData().then(res => {
+                this.recommends = res.data.data.recommend.list
+                this.banners = res.data.data.banner.list
+            })
+        }
     }
-  }
 </script>
 
 <style scoped>
-  .nav-bar{
+  .nav-bar {
     background-color: var(--color-tint);
     font-weight: 700;
     color: #fff;
