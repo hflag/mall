8a0a71bdc073fb3fd7e0e18c38d722f3179281ef
hflag <hflag@163.com>
2019-12-15 09:27:23 +0800

    '封装了featureView组件'

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,6 +5,59 @@
     </nav-bar>
     <my-swipe :banners="banners"/>
     <recommend-view :recommends="recommends"/>
+    <feature-view/>
+    <ul>
+      <li>列表1</li>
+      <li>列表2</li>
+      <li>列表3</li>
+      <li>列表4</li>
+      <li>列表5</li>
+      <li>列表6</li>
+      <li>列表7</li>
+      <li>列表8</li>
+      <li>列表9</li>
+      <li>列表10</li>
+      <li>列表11</li>
+      <li>列表12</li>
+      <li>列表13</li>
+      <li>列表14</li>
+      <li>列表15</li>
+      <li>列表16</li>
+      <li>列表17</li>
+      <li>列表18</li>
+      <li>列表19</li>
+      <li>列表20</li>
+      <li>列表21</li>
+      <li>列表22</li>
+      <li>列表23</li>
+      <li>列表24</li>
+      <li>列表25</li>
+      <li>列表26</li>
+      <li>列表27</li>
+      <li>列表28</li>
+      <li>列表29</li>
+      <li>列表30</li>
+      <li>列表31</li>
+      <li>列表32</li>
+      <li>列表33</li>
+      <li>列表34</li>
+      <li>列表35</li>
+      <li>列表36</li>
+      <li>列表37</li>
+      <li>列表38</li>
+      <li>列表39</li>
+      <li>列表40</li>
+      <li>列表41</li>
+      <li>列表42</li>
+      <li>列表43</li>
+      <li>列表44</li>
+      <li>列表45</li>
+      <li>列表46</li>
+      <li>列表47</li>
+      <li>列表48</li>
+      <li>列表49</li>
+      <li>列表50</li>
+    </ul>
   </div>
 </template>
 
@@ -13,6 +66,8 @@
     import NavBar from 'components/common/navbar/NavBar'
     import MySwipe from 'components/common/swipe/MySwipe'
     import RecommendView from "./childComps/RecommendView";
+    import FeatureView from "./childComps/FeatureView";
+
     import {getHomeMultiData} from "../../network/home";
 
     export default {
@@ -24,6 +79,7 @@
             }
         },
         components: {
+            FeatureView,
             RecommendView,
             NavBar,
             MySwipe
